dcd80be982aa9abdc2da7d0df22b8605d413bc8f
XavierJiezou <xuechaozou@foxmail.com>
2024-11-29 11:07:20 +0800

    add files

---
+++ configs/vegseg/experiment_m.py
@@ -0,0 +1,16 @@
+_base_ = [
+    "../_base_/models/vegseg.py",
+    "../_base_/datasets/grass.py",
+    "../_base_/default_runtime.py",
+    "../_base_/schedules/grass_schedule.py",
+]
+
+data_preprocessor = dict(size=(256, 256))
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(num_classes=5),
+    auxiliary_head=dict(num_classes=5),
+    veg_adapter=dict(
+        type="VegAdapter", in_channels=[768, 768, 768, 768], model_type="vitBlock"
+    ),
+)
--- vegseg/models/adapter/veg_adapter.py
+++ vegseg/models/adapter/veg_adapter.py
@@ -5,6 +5,7 @@ from torch.nn import functional as F
 from typing import Callable, Optional
 from torch import Tensor
 from timm.models.layers import trunc_normal_
+from timm.models.vision_transformer import Block as TransformerBlock
 
 
 class Mlp(nn.Module):
@@ -36,24 +37,57 @@ class Mlp(nn.Module):
 
 @MODELS.register_module()
 class VegAdapter(BaseModule):
-    def __init__(self, in_channels, rank_dim=4, mlp_nums=1, init_cfg=None):
+    def __init__(
+        self,
+        in_channels,
+        rank_dim=4,
+        mlp_nums=1,
+        model_type="mlp",
+        num_heads=8,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_norm=False,
+        init_values=None,
+        proj_drop_rate: float = 0.0,
+        attn_drop_rate: float = 0.0,
+        init_cfg=None,
+    ):
         super().__init__(init_cfg)
         self.adapters = nn.ModuleList()
-
-        for in_channel in in_channels:
-            mlp_list = []
-            for _ in range(mlp_nums):
-                mlp_list.append(
-                    Mlp(
-                        in_channel,
-                        hidden_features=in_channel // rank_dim,
-                        out_features=in_channel,
+        if model_type == "mlp":
+            for in_channel in in_channels:
+                mlp_list = []
+                for _ in range(mlp_nums):
+                    mlp_list.append(
+                        Mlp(
+                            in_channel,
+                            hidden_features=in_channel // rank_dim,
+                            out_features=in_channel,
+                        )
                     )
-                )
-            mlp_model = nn.Sequential(
-                *mlp_list
-            )
-            self.adapters.append(mlp_model)
+                mlp_model = nn.Sequential(*mlp_list)
+                self.adapters.append(mlp_model)
+
+        elif model_type == "vitBlock":
+            for in_channel in in_channels:
+                model_list = []
+                for _ in range(mlp_nums):
+                    model_list.append(
+                        TransformerBlock(
+                            in_channel,
+                            num_heads=num_heads,
+                            mlp_ratio=mlp_ratio,
+                            qkv_bias=qkv_bias,
+                            qk_norm=qk_norm,
+                            init_values=init_values,
+                            proj_drop=proj_drop_rate,
+                            attn_drop=attn_drop_rate,
+                        )
+                    )
+                self.adapters.append(nn.Sequential(*model_list))
+        
+        else:
+            raise ValueError(f"model type must in ['mlp','vitBlock'],actually is {model_type}")
 
         self.apply(self._init_weights)
 
